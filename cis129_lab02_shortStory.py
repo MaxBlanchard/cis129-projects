126fab1ef73ba77127c7c4b3f65c15bd17e33b90
MaxBlanchard <mblanchard7@mail.pima.edu>
2024-09-13 22:59:03 -0700

    Created a short story about my first day on the job

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+#This is my first real coding assignment is this cis129 course
+print('A Short Story by Maximilian Blanchard')
+#prints a title to the story
+print('    ')
+#prints a space to give the title some room
+print('My first day working at my job was one of the calmest and most stressful days of my life.')
+print('I had started in dining room which the area that is in charge of cleaning, you guessed it, the dining room.')
+print('This includes cleaning tables, floors, and highchairs; and it also includes cleaning the trays and taking out the trash.')
+print('Well most of my first day was pretty slow and boring until around the last hour of my shift.')
+print('I had noticed a bus pulling in and then another and then another.')
+print('These 3 buses had tons of sport players from baseball to football to soccer.')
+print('This had led to a huge rush which afterwards made the entire dinning room a mess leaving me who was supposed to have clocked out at 7 have to stay until 8.')
+print('What a fun first day on the job.')
+#prints a short story
