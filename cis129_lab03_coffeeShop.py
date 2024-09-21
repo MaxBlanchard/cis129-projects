23b512da9256e44ba5cc4a8c0575331447b6577b
MaxBlanchard <mblanchard7@mail.pima.edu>
2024-09-20 23:20:14 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -2,8 +2,13 @@
 priceCoffee = 5
 #price for muffin is 4$
 priceMuffin = 4
+#price for a Donut is 4$
+priceDonut = 4
+#price for a Soda is 3$
+priceSoda = 3
 #6% tax
 tax = .06
+
 #For formating the reciept
 print('****************************')
 
@@ -11,6 +16,8 @@ print('****************************')
 print("Max's Coffee Shop")
 coffeeBought = int(input("How Many Coffees did you buy? "))
 muffinBought = int(input("How Many Muffins did you buy? "))
+donutBought = int(input("How Many Donuts did you buy? "))
+sodaBought = int(input("How Many Sodas did you buy? "))
 
 #Gives spacing on the Reciept
 print('****************************')
@@ -20,20 +27,24 @@ print('****************************')
 #Creates totals based on the prices beofor
 totalCoffee = coffeeBought * priceCoffee
 totalMuffin = muffinBought * priceMuffin
+totalDonut = donutBought * priceDonut
+totalSoda = sodaBought * priceSoda
 
 #Gets the amount of tax owed from the previous totals
-totalTax = float(totalMuffin * tax) + float(totalCoffee * tax)
+totalTax = float(totalMuffin * tax) + float(totalCoffee * tax) + float(totalDonut * tax) + float(totalSoda * tax)
 
 #Returns the previous totals 
 print("Max's Coffee Shop Receipt")
 print('1 Coffee at 5$ each: $', totalCoffee)
 print('1 Muffin at 4$ each: $', totalMuffin)
+print('1 Donut at 4$ each: $', totalDonut)
+print('1 Soda at 3$ each: $', totalSoda)
 print('6% tax: ',totalTax)
 print('----------')
 
 #adds all the different costs together to give the total
-taxCoffee = (totalTax + totalCoffee)
-totalTota1 = totalMuffin + taxCoffee
+totalTotal = totalTax + totalCoffee + totalMuffin + totalDonut + totalSoda
 
-print('Total: $', str(totalTota1))
+#prints the total and gives and ending line
+print('Total: $', str(totalTotal))
 print('****************************')
